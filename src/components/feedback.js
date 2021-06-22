5b2f52a2074d34389a7bfea80207fee379c45466
splashhar <78307069+splashhar@users.noreply.github.com>
2021-06-22 06:54:58 +0530

    Add files via upload

---
+++ src/components/feedback.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './feedback.css'
+function Feedback(){
+    return <div>
+    <body>
+    <form class="form">
+  <h2>Feedback</h2>
+
+  <p type="Message:"><input placeholder="What would you like to suggest us.."></input></p>
+  <button>Submit Feedback</button>
+  <div>
+    <span class="fa fa-phone"></span>001 1023 567
+    <span class="fa fa-envelope-o"></span> Cpyright Turnskill
+  </div>
+</form>
+  </body>
+  </div>
+};
+
+export default Feedback;
